b934b770c53e0046fb18d6279cc7c24c706d7e87
折田幸夫 <3204704@gmail.com>
2020-04-04 18:32:20 +0900

    初期TABLEを追加したい場合はコメントアウト外す。

--- mysql/db/init.sql
+++ mysql/db/init.sql
@@ -1,13 +1,18 @@
 
-/*以下Databaseの内容が記載される
+-- 以下Databaseの内容が記載される
 
-CREATE DATABASE mysql_db;
-use mysql_db;
+-- CREATE DATABASE mysql_db;
+-- use mysql_db;
 
-CREATE TABLE users (
-  id int(10) unsigned not null auto_increment,
-  name varchar(255) not null,
-  created_time datetime not null default current_timestamp,
-  updated_time datetime not null default current_timestamp on update current_timestamp,
-  primary key (id)
-);
+-- CREATE TABLE users
+-- (
+--   id int(10)
+--   unsigned not null auto_increment,
+--   name varchar
+--   (255) not null,
+--   created_time datetime not null default current_timestamp,
+--   updated_time datetime not null default current_timestamp on
+--   update current_timestamp,
+--   primary key
+--   (id)
+-- );
